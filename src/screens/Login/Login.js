437e8bb1b5856d6f972ca5f8368f048cacc2b216
Abdullah Talakey <abdullah.talakey@gmail.com>
2018-07-21 16:55:29 +0300

    Create reusable and configurable container component

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import { ImageBackground, StyleSheet, View } from 'react-native';
-import { Container, Content, Form, Item, Input, Label, Button, Text } from 'native-base';
+import { StyleSheet, View } from 'react-native';
+import { Content, Form, Item, Input, Label, Button, Text } from 'native-base';
 import { startSearchScreen } from '../../../index';
 
-import backgroundImage from '../../assets/background.jpg';
+import MainContainer from '../../components/MainContainer/MainContainer';
+import backgroundImage from '../../assets/loginBackground.jpg';
 
 class LoginScreen extends Component {
   static navigatorStyle = {
@@ -19,40 +20,34 @@ class LoginScreen extends Component {
 
   render() {
     return (
-      <Container>
-        <ImageBackground style={styles.backgroundImage} source={backgroundImage}>
-          <Content contentContainerStyle={styles.container}>
-            <Form>
-              <Item floatingLabel>
-                <Label>Username</Label>
-                <Input />
-              </Item>
-              <Item floatingLabel last>
-                <Label>Password</Label>
-                <Input />
-              </Item>
-            </Form>
-            <View style={styles.buttonContainer}>
-              <Button transparent light onPress={this.loginHandler}>
-                <Text>Sign In</Text>
-              </Button>
-            </View>
-          </Content>
-        </ImageBackground>
-      </Container>
+      <MainContainer imageBackgroundSource={backgroundImage}>
+        <Content contentContainerStyle={styles.content}>
+          <Form>
+            <Item floatingLabel>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+          </Form>
+          <View style={styles.buttonContainer}>
+            <Button transparent light onPress={this.loginHandler}>
+              <Text>Sign In</Text>
+            </Button>
+          </View>
+        </Content>
+      </MainContainer>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  content: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center'
-  },
-  backgroundImage: {
-    flex: 1,
-    width: '100%'
   },
   buttonContainer: {
     flexDirection: 'row',
--- src/screens/Search/Search.js
+++ src/screens/Search/Search.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
-import { Container, Header, Item, Input, Icon } from 'native-base';
+import { Header, Item, Input, Icon } from 'native-base';
 
-import backgroundImage from '../../assets/background.jpg';
+import MainContainer from '../../components/MainContainer/MainContainer';
 
 class SearchScreen extends Component {
   static navigatorStyle = {
@@ -11,25 +10,16 @@ class SearchScreen extends Component {
 
   render() {
     return (
-      <Container>
-        <ImageBackground style={styles.backgroundImage} source={backgroundImage}>
-          <Header transparent searchBar rounded>
-            <Item>
-              <Icon name="ios-search" />
-              <Input placeholder="Search" />
-            </Item>
-          </Header>
-        </ImageBackground>
-      </Container>
+      <MainContainer>
+        <Header transparent searchBar rounded>
+          <Item>
+            <Icon name="ios-search" />
+            <Input placeholder="Search" />
+          </Item>
+        </Header>
+      </MainContainer>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  backgroundImage: {
-    flex: 1,
-    width: '100%'
-  }
-});
 
 export default SearchScreen;
