af4bad4d699a3003839781bd320662002065b351
Abdullah Talakey <abdullah.talakey@gmail.com>
2018-07-23 08:09:28 +0300

    Housekeeping: 1. Use a bigger font size for a planet with larger population by normalizing its population to scale to a number between 16 and 40 and using it as the font size. 2. Hide logout button and disable search bar on logout.

--- src/screens/Search/Search.js
+++ src/screens/Search/Search.js
@@ -11,13 +11,26 @@ export default class SearchScreen extends Component {
     super(props);
     this.state = {
       isLoading: false,
+      isLoggingOut: false,
       data: {
         nextUrl: '',
         planets: []
-      }
+      },
+      minPopulation: 0,
+      maxPopulation: 0
     };
 
     this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+  }
+
+  componentDidMount = () => {
+    this._interval = setInterval(() => {
+      // Your code
+    }, 5000);
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this._interval);
   }
 
   onNavigatorEvent = (event) => { // this is the onPress handler for the buttons
@@ -30,20 +43,25 @@ export default class SearchScreen extends Component {
 
   logoutHandler = () => {
     Keyboard.dismiss();
-    this.toggleIsLoading();
-    setTimeout(() => startLoginScreen(), 2000);
+    this.props.navigator.setButtons({rightButtons:[]})
+    this.toggleIsLoading(true);
+    setTimeout(() => startLoginScreen(), 1500);
   }
 
   searchInputOnChangeTextHandler = (searchStr) => {
-    let url = `https://swapi.co/api/planets/?search=${searchStr}`;
-    this.toggleIsLoading();
-    this.fetchData(url).then((data) => {
-      this.updateDataState(data, true);
+    if (searchStr !== '') {
+      let url = `https://swapi.co/api/planets/?search=${searchStr}`;
       this.toggleIsLoading();
-    }).catch((error) => {
-      console.log('Error:', error)
-      this.toggleIsLoading();
-    });;
+      this.fetchData(url).then((data) => {
+        this.updateDataState(data, true);
+        this.toggleIsLoading();
+      }).catch((error) => {
+        console.log('Error:', error)
+        this.toggleIsLoading();
+      });
+    } else {
+      this.updateDataState({next: '', results: []}, true);
+    }
   }
 
   flatListOnEndReachedHandler = () => {
@@ -73,34 +91,61 @@ export default class SearchScreen extends Component {
     }).catch(error => console.log('Error fetching data:', error));
   }
 
-  toggleIsLoading = () => {
+  toggleIsLoading = (disableSrearchBar) => {
     this.setState(previousState => {
       return {
         ...previousState,
-        isLoading: !previousState.isLoading
+        isLoading: !previousState.isLoading,
+        isLoggingOut: disableSrearchBar ? disableSrearchBar : false
       }
     });
   }
 
   updateDataState = (data, overwrite) => {
     this.setState(previousState => {
-      let nextUrl = data.next;
-      let planets = data.results;
-      if (overwrite === false) {
-        const allPlanets = previousState.data.planets.concat(planets);
-        const planetNames = allPlanets.map(planet => planet.name);
-        planets = allPlanets.filter((planet, index) => {
-          return planetNames.indexOf(planet.name) === index;
-        });
-      }
-
+      const nextUrl = data.next;
+      const planets = this.getFilteredPlanetsArray(previousState.data.planets , data.results, overwrite);
+      const planetPopulations = this.getPopulationNumbersArray(planets);
       return {
         data: {
           nextUrl: nextUrl ? nextUrl : '',
           planets: planets
-        }
+        },
+        minPopulation: Math.min(...planetPopulations),
+        maxPopulation: Math.max(...planetPopulations)
       }
     });
+  }
+
+  getFilteredPlanetsArray = (previousStatePlanetsData, planetsData, overwrite) => {
+    let planets = planetsData;
+    if (overwrite === false) {
+      const allPlanets = previousStatePlanetsData.concat(planetsData);
+      const planetNames = allPlanets.map(planet => planet.name);
+      planets = allPlanets.filter((planet, index) => {
+        return planetNames.indexOf(planet.name) === index;
+      });
+    }
+    return planets;
+  }
+
+  getPopulationNumbersArray = (planets) => {
+    return planets.map(planet => planet.population)
+      .filter((stringPopulation) => stringPopulation !== 'unknown')
+      .map((stringPopulation) => parseInt(stringPopulation));
+  }
+
+  getTextFontSize = (population) => {
+    const minFontSize = 16;
+    const maxFontSize = 40;
+    let textFontSize = minFontSize;    
+    if(typeof population !== 'unknown')
+    {
+      const numerator = (parseInt(population) - this.state.minPopulation) * (maxFontSize - minFontSize);
+      const denominator = this.state.maxPopulation - this.state.minPopulation;
+      textFontSize = Math.round(minFontSize + numerator / denominator);
+    }
+    return textFontSize;
   }
 
   render() {
@@ -120,14 +165,14 @@ export default class SearchScreen extends Component {
             <Card>
               <CardItem button onPress={() => this.cardItemOnPressHandler(item)}>
                 <Body>
-                  <Text>
+                  <Text style={{fontSize: this.getTextFontSize(item.population)}}>
                     {item.name}
                   </Text>
                 </Body>
               </CardItem>
             </Card>
           )}
-          onEndReachedThreshold={0.5}
+          onEndReachedThreshold={0.2}
           onEndReached={this.flatListOnEndReachedHandler}
         />
       );
@@ -138,7 +183,7 @@ export default class SearchScreen extends Component {
         <Header transparent searchBar rounded>
           <Item>
             <Icon name="ios-search" />
-            <Input placeholder="Search" onChangeText={this.searchInputOnChangeTextHandler} />
+            <Input disabled={this.state.isLoggingOut} placeholder="Search" onChangeText={this.searchInputOnChangeTextHandler} />
           </Item>
         </Header>
         <View style={this.state.isLoading ? styles.content : styles.flatListContainer}>
